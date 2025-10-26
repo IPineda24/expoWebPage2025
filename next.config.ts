5d271b13273e9e9bb18490eb516649928f75521c
Irvin Pineda <00024420@uca.edu.sv>
2025-10-26 13:55:54 -0600

    FixBug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 
-const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+    formats: ['image/avif', 'image/webp'],
+    dangerouslyAllowSVG: true, // ← Permitir SVGs
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
